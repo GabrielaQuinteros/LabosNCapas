a635a61b5a6a7357f94c577c645655abf4898f51
GabrielaQuinteros <00060422@uca.edu.sv>
2025-04-05 17:09:08 -0600

    :sparkles:Laboratorio 01 N-Capas

--- Laboratorio01/src/Main.java
+++ Laboratorio01/src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Iniciando el Sistema de Citas Médicas del Hospital Nacional de Zaun");
+        System.out.println("Iniciando el Sistema de Citas Médicas del Hospital Nacional De Zaun");
 
         // Iniciar el sistema
         View.MainView mainView = new View.MainView();
